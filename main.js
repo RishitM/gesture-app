215e65fe3614912b4d6fa6c3a622856946edff14
RishitM <72683716+RishitM@users.noreply.github.com>
2021-02-24 08:59:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,30 @@ Webcam.attach("camera");
 
 function TakeImg(){
     Webcam.snap(function(data_uri){
-    document.getElementById("result").innerHTML="<img src="+data_uri+"></img>";
+    document.getElementById("result").innerHTML="<img id='new_pic' src="+data_uri+"></img>";
     });
-    
+}
+console.log("ml5 version",ml5.version);
+classifier= ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/hputIlhyd/model.json",
+modelLoaded);
+
+function modelLoaded(){
+    console.log("The Model Is Loaded!");
+};
+function IdentifyImg(){
+img= document.getElementById("new_pic");
+classifier.classify(img ,
+    got_Result)
+}
+function got_Result(error,result){
+   if (error){
+       console.error("3rr0r");
+   } 
+   else {
+       console.log(result);
+    Name1=result[0].label;
+       document.getElementById("name1").innerHTML= Name1;
+
+
+   }
 }
